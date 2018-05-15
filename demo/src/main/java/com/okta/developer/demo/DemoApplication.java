c2465f6ad097075c9ffd5ed34d88d87e40825c85
YannickPire <yannick.pire@telenet.be>
2018-05-15 21:26:40 +0200

    Simple Syso Example - YP

--- demo/src/main/java/com/okta/developer/demo/DemoApplication.java
+++ demo/src/main/java/com/okta/developer/demo/DemoApplication.java
@@ -1,12 +1,32 @@
 package com.okta.developer.demo;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.okta.developer.demo.model.Car;
+import com.okta.developer.demo.repository.CarRepository;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(CarRepository carRepository) {
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti", "AMC Gremlin", "Triumph Stag",
+					"Ford Pinto", "Yugo GV").forEach(name -> {
+						Car car = new Car();
+						car.setName(name);
+						carRepository.save(car);
+					});
+			carRepository.findAll().forEach(System.out::println);
+		};
 	}
 }
