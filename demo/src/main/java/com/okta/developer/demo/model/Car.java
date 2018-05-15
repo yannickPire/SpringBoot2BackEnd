cf8a5f81a815c03c84a151b69eef497bc581ab02
YannickPire <yannick.pire@telenet.be>
2018-05-15 21:13:09 +0200

    Created model class - YP

---
+++ demo/src/main/java/com/okta/developer/demo/model/Car.java
@@ -0,0 +1,27 @@
+package com.okta.developer.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Car {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private @NonNull String name;
+
+}
