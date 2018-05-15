ab4cddfc5df50e90da77bf4d5bc20a72f62df0a2
YannickPire <yannick.pire@telenet.be>
2018-05-15 21:26:16 +0200

    Remove lombok getters/setters & toString method and generated its own - YP

--- demo/src/main/java/com/okta/developer/demo/model/Car.java
+++ demo/src/main/java/com/okta/developer/demo/model/Car.java
@@ -5,17 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
-@Getter
-@Setter
 @NoArgsConstructor
-@ToString
 @EqualsAndHashCode
 public class Car {
 
@@ -23,5 +17,26 @@ public class Car {
 	@GeneratedValue
 	private Long id;
 	private @NonNull String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Car(id=" + getId() + ", name=" + getName() + ")";
+	}
 
 }
